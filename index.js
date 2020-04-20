91f22f280cc65ee1a6f636d4b2bb79230493fdaf
Shreyans Sonthalia <ssshreyans26@gmail.com>
2020-04-20 22:48:06 +0530

    second one

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import expressGraphQL from 'express-graphql';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import graphQLSchema from './graphql/schema';
+import graphQLResolvers from './graphql/resolvers';
+require('dotenv').config();
+const app = express();
+app.use(
+  cors(),
+  bodyParser.json()
+)
+app.use(
+    "/graphql",
+    expressGraphQL({
+      schema: graphQLSchema,
+      rootValue: graphQLResolvers,
+      graphiql: true
+    })
+  );
+function main() {
+  const port = process.env.PORT || 5000;
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-szwuh.mongodb.net/test?retryWrites=true&w=majority`
+
+  mongoose.connect(uri, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
+
+main();
